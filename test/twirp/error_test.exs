350d0e0627aea7ed7ef8b5ceef537482ba4ebdd5
Chris Keathley <c@keathley.io>
2020-07-07 10:44:31 -0400

    Add additional tests for raising exceptions

--- test/twirp/error_test.exs
+++ test/twirp/error_test.exs
@@ -9,4 +9,10 @@ defmodule Twirp.ErrorTest do
       assert "I can't make a hat that small!" == Exception.message(error)
     end
   end
+
+  test "can raise exception" do
+    assert_raise Twirp.Error, ~r|hat that small|, fn ->
+      raise Error.invalid_argument("I can't make a hat that small!")
+    end
+  end
 end
